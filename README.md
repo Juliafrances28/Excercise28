13228211d8dc84155b5586abe79daad28290d935
Julia Horan <julia.horan28@gmail.com>
2021-01-18 15:12:17 -0500

    deloying

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ The user should be able to:
 
 To deploy an application with a MongoDB database to Heroku, you'll need to set up a MongoDB Atlas account and connect a database from there to your application. Be sure to use the following guides for support:
 
-- [Set Up MongoDB Atlas](../04-Important/MongoAtlas-Setup.md)
+- [Set Up MongoDB Atlas]()
 
 - [Deploy with Heroku and MongoDB Atlas](../04-Important/MongoAtlas-Deploy.md)
 
--- server.js
+++ server.js
@@ -12,10 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongodb + srv; //dbworkout:<Peterrabbit2020!>@cluster0.ft9nt.mongodb.net/<dbname>?retryWrites=true&w=majority
-
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/workoutdb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 require("./routes/htmlroutes.js")(app);
